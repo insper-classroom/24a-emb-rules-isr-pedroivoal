8b6eb58b6ee1906e165e69858fd2a60a6181ad35
Rafael Corsi <corsiferrao@gmail.com>
2024-02-20 11:40:46 -0300

    update for rasp pico

--- main_isr_practice.c
+++ main_isr_practice.c
@@ -1,64 +1,44 @@
-#include "asf.h"
+#include "hardware/gpio.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
 
-#define LED_PIO PIOC
-#define LED_PIO_ID ID_PIOC
-#define LED_IDX 8
-#define LED_IDX_MASK (1 << LED_IDX)
-
-#define BUT_PIO PIOA
-#define BUT_PIO_ID ID_PIOA
-#define BUT_IDX 11
-#define BUT_IDX_MASK (1 << BUT_IDX)
-
-void io_init(void);
-void pisca_led(int n, int t);
-void btn_callback(void);
+const int BTN_PIN_R = 28;
 
 // ERRO: cppcheck unitvar
 // ERRO: addon IsrVarVolatile
 int btn_flag;
 
-// ERRO: addon IsrFuncName
-void btn_callback(void) {
+void btn_callback(uint gpio, uint32_t events) {
+  if (events == 0x4) { // fall edge
 
-  // ERRO: addon IsrPrintf
-  printf("btn pressed \n");
+    // ERRO: addon IsrPrintf
+    printf("btn pressed \n");
 
-  // ERRO: addon IsrNoLoop
-  while (!pio_get(BUT_PIO, BUT_IDX_MASK)) {
+    // ERRO: addon IsrNoLoop
+    while (!pio_get(BTN_PIN_R)) {
+      // ERRO: addon IsrNoDelay
+      sleep_ms(1);
+    }
+
+    // ERRO: addon IsrPrintf
+    printf("btn released \n");
+
     // ERRO: addon IsrNoDelay
-    delay_ms(1);
+    sleep_ms(1);
+    btn_flag = 1;
   }
-
-  // ERRO: addon IsrPrintf
-  printf("btn released \n");
-
-  // ERRO: addon IsrNoDelay
-  delay_ms(1);
-
-  btn_flag = 1;
 }
 
-void io_init(void) {
-  pmc_enable_periph_clk(LED_PIO_ID);
-  pio_configure(LED_PIO, PIO_OUTPUT_0, LED_IDX_MASK, PIO_DEFAULT);
-  pmc_enable_periph_clk(BUT_PIO_ID);
-  pio_configure(BUT_PIO, PIO_INPUT, BUT_IDX_MASK, PIO_PULLUP);
-  pio_handler_set(BUT_PIO, BUT_PIO_ID, BUT_IDX_MASK, PIO_IT_FALL_EDGE,
-                  btn_callback);
-  pio_enable_interrupt(BUT_PIO, BUT_IDX_MASK);
-  pio_get_interrupt_status(BUT_PIO);
-  NVIC_EnableIRQ(BUT_PIO_ID);
-  NVIC_SetPriority(BUT_PIO_ID, 4);
-}
+int main() {
+  stdio_init_all();
+  gpio_init(BTN_PIN_R);
+  gpio_set_dir(BTN_PIN_R, GPIO_IN);
+  gpio_pull_up(BTN_PIN_R);
+  gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
+                                     &btn_callback);
 
-void main(void) {
-  sysclk_init();
-  WDT->WDT_MR = WDT_MR_WDDIS;
-  io_init();
   volatile int capture_flag = 0;
   int a;
-
   while (1) {
     if (btn_flag) {
       capture_flag = 1;
@@ -67,5 +47,6 @@ void main(void) {
 
     if (capture_flag) {
     }
+    
   }
 }
